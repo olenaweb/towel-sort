b2772b3fc3377f4676d96b39be2187e7fb9dc62a
olenaweb <olena.nevzorova@gmail.com>
2022-08-06 20:08:41 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  function compareNumbers(a, b) {
+    return b - a;
+  }
+  let result = [];
+  if (matrix == undefined) {
+    return [];
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    if ((i + 1) % 2 == 0) {
+      matrix[i].sort(compareNumbers);
+    }
+    result = result.concat(matrix[i]);
+  }
+  return result;
 }
